dea20a612e9ef8dad3e78b950ce9af33a6275ead
suresh Daneti <sureshdaneti99@gmail.com>
2021-10-16 12:26:41 +0530

    Added: User need to enter a mobile number in pre defined format

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 import java.util.Scanner;
 /**
- * Purpose  - As a User need to enter a valid Email ID
+ * Purpose  - As a User need to enter a mobile number in a pre-defined format.
  */
 
 public class UserRegistrationMain {
@@ -17,6 +17,7 @@ public class UserRegistrationMain {
 		System.out.println("Enter 1 : to validate First Name ");
 		System.out.println("Enter 2 : to validate Last Name ");
 		System.out.println("Enter 3 : to validate E-mail Address ");
+		System.out.println("Enter 4 : to validate Mobile Number ");
 		switch (scanner.nextInt()){
 		case 1:
 			UserRegistration.validFirstName();
@@ -26,6 +27,9 @@ public class UserRegistrationMain {
 			break;
 		case 3:
 			UserRegistration.validEmailId();
+			break;
+		case 4:
+			UserRegistration.validMobileNumber();
 			break;
 		default:
 			System.out.println("Select a valid number");
